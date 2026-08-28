95d3f55b623d676ae0bd2b3dc49f70e5c0870948
Gabriel Oliveira <gabrielerika8724@gmail.com>
2026-08-28 18:55:29 -0300

    Adiciona Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+/* =========================
+   src/App.tsx
+========================= */
+
+import { Analytics } from '@vercel/analytics/react';
 import { AppRoutes } from './routes/AppRoutes';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
